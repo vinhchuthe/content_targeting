e7e04a3b287d7614b0a7a4bdbfda71a2468d4de2
VinhChu <vinhchuthe@admicro.vn>
2021-01-08 17:55:36 +0700

    update project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,117 @@
 // ------ particlesJS --------
+particlesJS('particles-js0',
+    {
+        "particles": {
+            "number": {
+                "value": 6,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": ["#BD10E0", "#B8E986", "#50E3C2", "#FFD300", "#E86363"]
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000"
+                },
+                "polygon": {
+                    "nb_sides": 6
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 1,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 7,
+                "random": false,
+                "anim": {
+                    "enable": true,
+                    "speed": 10,
+                    "size_min": 40,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": false,
+                "distance": 200,
+                "color": "#ffffff",
+                "opacity": 1,
+                "width": 2
+            },
+            "move": {
+                "enable": true,
+                "speed": 4,
+                "direction": "top",
+                "random": true,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": false,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": false,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    }
+);
+
 particlesJS('particles-js1',
     {
         "particles": {
@@ -492,7 +605,7 @@ $(".popup-close").click(function () {
 // -------------- Profile ----------------------
 
 // Range Slider
-var valMap = ["2.5k", "5k", "10k", "15k", "20k"];
+var valMap = ["1k", "2k", "5k", "10k", "15k", "20k"];
 var rng = document.getElementById("rng");
 var ro = document.getElementById("tip");
 var tooltip = document.getElementById("tooltip");
@@ -500,12 +613,37 @@ var prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
 
 
 function updateRange() {
+    var num = rng.value,
+        tab;
+    console.log(num);
     ro.textContent = valMap[parseInt(rng.value, 10)];
     // console.log("Selected value is: " + valMap[parseInt(rng.value, 10)] + ", Associated value is: " + rng.value);
+
+    if (num == 0) {
+        tab = "tab4";
+        console.log(tab);
+    } else if (num == 1) {
+        tab = "tab4";
+        console.log(tab);
+    } else if (num == 2) {
+        tab = "tab1";
+        console.log(tab);
+    } else if (num == 3) {
+        tab = "tab2";
+        console.log(tab);
+    } else if (num == 4) {
+        tab = "tab3";
+        console.log(tab);
+    } else if (num == 5) {
+        tab = "tab4";
+        console.log(tab);
+    }
+
 }
 
 function setBubble(range, bubble) {
     const val = range.value;
+    console.log(val);
     const min = range.min ? range.min : 0;
     const max = range.max ? range.max : 100;
     const newVal = Number(((val - min) * 100) / (max - min));
@@ -515,23 +653,29 @@ function setBubble(range, bubble) {
     bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
 }
 
+function grad() {
+    var bval = parseInt(rng.value, 10);
+    var pos = (bval - rng.getAttribute('min')) / (rng.getAttribute('max') - rng.getAttribute('min'));
+    pos = Math.round(pos * 99);
+    var grad = 'linear-gradient(90deg, #FFEB3B ' + pos + '%,#929496 ' + (pos + 1) + '%)';
+    rng.style.background = grad;
+}
+
 
 window.addEventListener("DOMContentLoaded", updateRange);
 rng.addEventListener("input", updateRange);
 rng.addEventListener("input", () => {
     setBubble(rng, tooltip);
 });
-rng.addEventListener("input", () => {
-    var bval = parseInt(rng.value, 10);
-    var pos = (bval - rng.getAttribute('min')) / (rng.getAttribute('max') - rng.getAttribute('min'));
-    pos = Math.round(pos * 99);
-    var grad = 'linear-gradient(90deg, #FFEB3B ' + pos + '%,#929496 ' + (pos + 1) + '%)';
-    rng.style.background = grad;
-})
+rng.addEventListener("input", grad);
 
 // Tab
 $(".pricePlan--header-title").click(function () {
     var tab = $(this).attr("title-id");
+    $("#rng").val($(this).attr("range"));
+    updateRange();
+    setBubble(rng, tooltip);
+    grad();
     $(".pricePlan--header-title").removeClass("header-active");
     $(this).addClass("header-active");
     $(".tab-content").removeClass("tab-active");
@@ -591,12 +735,21 @@ $("#banner").on('mousemove', moveCircle);
 
 // ------------------- feature ----------------------
 $(".grid-list-item").on("mouseenter", function () {
-    $(".grid-list-item").removeClass("item-active");
-    $(this).addClass("item-active");
-    var feature_img = $(this).attr("data-id");
-    // console.log(feature_img);
-    $(".grid-img").removeClass("img-active");
-    $("#" + feature_img).addClass("img-active");
+    if ($(this).hasClass("item-active")) {
+        console.log("already active");
+    } else {
+        $(".grid-list-item").removeClass("item-active");
+        $(this).addClass("item-active");
+        var feature_img = $(this).attr("data-id");
+        $("#fgrid-img img").addClass("hide-img");
+        setTimeout(function () {
+            $("#fgrid-img img").attr("src", feature_img);
+        }, 400);
+        setTimeout(function () {
+            $("#fgrid-img img").removeClass("hide-img");
+        }, 600);
+
+    }
 });
 
 
@@ -604,6 +757,43 @@ $(".grid-list-item").on("mouseenter", function () {
 $(".navbar-item-link").click(function () {
     $(".sub-menu").toggleClass("active");
 });
+
+$(window).scroll(function () {
+    //get current sroll position
+    var scrollPosition = $(window).scrollTop();
+    //get the position of the containers
+    var banner = $("#banner").offset().top,
+        issue = $("#issue").offset().top,
+        scroll = $("#scroll").offset().top,
+        feature = $("#feature").offset().top,
+        baogia = $("#baogia").offset().top;
+
+    if (scrollPosition >= banner) {
+        $(".navbar-item-link").html("Content Targeting");
+        $(".sub-menu-item").siblings().removeClass("hide-sub");
+    }
+    if (scrollPosition >= issue) {
+        $(".navbar-item-link").html("Vấn đề");
+        $(".sub-menu-item").siblings().removeClass("hide-sub");
+        $("#sub-menu1").addClass("hide-sub");
+    }
+    if (scrollPosition >= scroll) {
+        $(".navbar-item-link").html("Giải pháp");
+        $(".sub-menu-item").siblings().removeClass("hide-sub");
+        $("#sub-menu2").addClass("hide-sub");
+    }
+    if (scrollPosition >= feature) {
+        $(".navbar-item-link").html("Điểm nổi bật");
+        $(".sub-menu-item").siblings().removeClass("hide-sub");
+        $("#sub-menu3").addClass("hide-sub");
+    }
+    if (scrollPosition >= baogia) {
+        $(".navbar-item-link").html("Báo giá");
+        $(".sub-menu-item").siblings().removeClass("hide-sub");
+        $("#sub-menu4").addClass("hide-sub");
+    }
+
+})
 
 var nav_link = $(".sub-menu-item-link");
 $(".sub-menu-item-link").on("click", function () {
@@ -615,7 +805,7 @@ $(".sub-menu-item-link").on("click", function () {
 
     TweenMax.to($(window), 1, {
         scrollTo: {
-            y: topY,
+            y: topY + 5,
             autoKill: true
         },
         ease: Power3.easeOut
